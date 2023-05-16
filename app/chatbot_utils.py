19482a11b1d70c915c274b883b634023e4cdb4f6
“jdhl27git config --global user.email independientemedellin.27.03@gmail.comgit config --global user.name “jdhl27 <independientemedellin.27.03@gmail.com>
2023-05-16 13:23:18 -0500

    add method to lowercase initial data

--- app/chatbot_utils.py
+++ app/chatbot_utils.py
@@ -10,3 +10,18 @@ nltk.download('punkt')
 
 # Carga del archivo CSV
 data = pd.read_csv("datos_tdea.csv")
+
+# Minuscula en todos los datos para el proceso exitoso
+def procesar_datos_iniciales(string, value = False):
+  if value:  
+    return [elemento.lower() for elemento in list(set(";".join(data[string]).split(";")))]
+
+  return [elemento.lower() for elemento in list(set(data[string]))]
+  
+
+# Preprocesamiento de los datos
+facultades = procesar_datos_iniciales("Facultad")
+carreras = procesar_datos_iniciales("Carrera")
+precios = procesar_datos_iniciales("Precio")
+semestres = procesar_datos_iniciales("Semestre")
+materias = procesar_datos_iniciales("Materias", True)
